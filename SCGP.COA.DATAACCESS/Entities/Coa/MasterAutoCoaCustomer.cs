aa77e4b041dad40e6cc2b03511f1a0c48d6e7169
Jabavokza <43809564+Jabavokza@users.noreply.github.com>
2022-10-24 14:50:49 +0700

    Update
    Update

--- SCGP.COA.DATAACCESS/Entities/Coa/MasterAutoCoaCustomer.cs
+++ SCGP.COA.DATAACCESS/Entities/Coa/MasterAutoCoaCustomer.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SCGP.COA.DATAACCESS.Entities.Coa
 {
     public partial class MasterAutoCoaCustomer
     {
+       
         public string CustomerCode { get; set; } = null!;
         public string? ShiptoCode { get; set; }
         public bool AutocoaActive { get; set; }
--- SCGP.COA.DATAACCESS/Entities/Coa/MasterCustomer.cs
+++ SCGP.COA.DATAACCESS/Entities/Coa/MasterCustomer.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SCGP.COA.DATAACCESS.Entities.Coa
 {
+    [Table("MASTER_CUSTOMER")]
     public partial class MasterCustomer
     {
         public string CustomerCode { get; set; } = null!;
